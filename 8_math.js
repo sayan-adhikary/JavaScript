62e39e26b7e7bacc7ecdebc7ef391cfa1a164ce9
sayan-adhikary <pap9679429830@gmail.com>
2025-01-21 23:50:00 +0530

    Create 8_math.js

---
+++ 8_math.js
@@ -0,0 +1,13 @@
+const score = 100
+console.log(score);
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toString());
+console.log(typeof balance);
+console.log(balance.toFixed(2));
+
+const otherNumber = 100.123456 
+console.log(otherNumber.toPrecision(4));
+
+
