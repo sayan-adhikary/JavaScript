726f4087d92397dc50ce54767b4f6b7a0a2583d7
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-02-17 11:10:36 +0530

    Updated

--- Hitesh_vai/02_another/4_object.js
+++ Hitesh_vai/02_another/4_object.js
@@ -18,13 +18,13 @@ const regularUser = {
 
 // console.log(regularUser.fullName);
 
-const object1 = {1: "a", 2: "b", 3: "c"};
+const object1 = {1: "a", 2: "b", 3: "c", 4: "d"};
 const object2 = {4: "a", 5: "b", 6: "c"};
 
 // const object3 = {object1,object2};
 // const object3 = Object.assign({}, object1, object2);       //Object.assign use for: merge two object
 
-const object3 = {...object1, ...object2};
+// const object3 = {...object1, ...object2};                       //Spread operator use for: merge two object, but it is more modern and cleaner than Object.assign
 // console.log(object3);
 
 
@@ -43,13 +43,13 @@ const users = [
     }
 ]
 
-users[1].email
-// console.log(users[1].email);
+// users[1].email
+// console.log(users);
 
 // console.log(tinderUser);
 
 
-// console.log(Object.keys(tinderUser));        //Object.keys use for: convert object to array
-// console.log(Object.values(tinderUser));      //Object.values use for: convert object to array
-// console.log(Object.entries(tinderUser));     //Object.entries use for: convert object to array
+console.log(Object.keys(tinderUser));        //Object.keys use for: convert object to array
+console.log(Object.values(tinderUser));      //Object.values use for: convert object to array
+console.log(Object.entries(tinderUser));     //Object.entries use for: convert object to array
 console.log(tinderUser.hasOwnProperty("name"));        //hasOwnProperty use for: check the property is exist or not
