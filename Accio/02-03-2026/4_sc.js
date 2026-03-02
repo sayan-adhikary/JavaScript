f8c743215e5606231925030598b00d3a121cbf9c
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-03-03 01:06:51 +0530

    Update 4_sc.js

--- Accio/02-03-2026/4_sc.js
+++ Accio/02-03-2026/4_sc.js
@@ -2,7 +2,12 @@ let arr = new Array(3);
 console.log(arr);
 
 
-function solve(arr){    //sc = O(n^2)
+function solve(arr) {    //sc = O(n^2)
     let a = arr.length;
-    let b = new Array(a*a);
+    let b = new Array(a * a);
 }
+/*
+new Array(n) → O(n)
+new Array(n*n) → O(n²)
+new Array(n*n*n) → O(n³) 
+*/
