2572f2bde448fc972d3b27bcdb2831ed72827063
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-03-13 18:24:13 +0530

    Update selection.js

--- Accio/3.March/07-03-2026/selection.js
+++ Accio/3.March/07-03-2026/selection.js
@@ -1,30 +1,6 @@
-function selectionsort(arr) {
-    let start = 0;
-    let end = arr.length - 1;
-    while (start < end) {
-        let temp = minFind(arr, start);
-        let temp2 = arr[start];
-        arr[start] = arr[temp];
-        arr[temp] = temp2;
-        start++;
-    }
-    return arr;
-}
-
-function minFind(arr, start) {
-    let minIdx = start;
-    for (let i = start; i < arr.length; i++) {
-        if (arr[i] < minIdx) {
-            minIdx = i;
-        }
-    }
-    return minIdx;
-}
-
 function selectionSort(arr) {
     let n = arr.length;
     for (let i = 0; i < n - 1; i++) {
-        let idx = i;
         let minIdx = i;
         for (let j = i; j < n; j++) {
             if (arr[j] < arr[minIdx]) {
@@ -32,13 +8,13 @@ function selectionSort(arr) {
             }
         }
 
-        // swap idx - minIdx
-        let temp = arr[idx];
-        arr[idx] = arr[minIdx];
+        // swap i -> minIdx
+        let temp = arr[i];
+        arr[i] = arr[minIdx];
         arr[minIdx] = temp;
     }
     return arr;
 }
 
-let arr = [60, 42, 31, 7, 12, 15];
+let arr = [7 ,4 ,1 ,5 ,3];
 console.log(selectionSort(arr));
