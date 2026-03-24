35175fba0b383e65d84d1ad10cfbc26e5351fca6
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-03-24 23:30:37 +0530

    Update memorization.js

--- Accio/3.March/22-03-2026/memorization.js
+++ Accio/3.March/22-03-2026/memorization.js
@@ -1,11 +1,11 @@
-function fibo(n){
-    if(n === 1){
+function fibo(n) {
+    if (n === 1) {
         return 1;
     }
-    if(n === 0){
+    if (n === 0) {
         return 0;
     }
-    if(dp[n] !== -1){
+    if (dp[n] !== -1) {
         return dp[n];
     }
 
