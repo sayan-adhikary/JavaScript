dcd132e1f80852cbda739634bf6b517aa3910969
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-05-17 12:36:39 +0530

    17-05-26

---
+++ Accio/DEVLOPMENT/2.May/9. Event Advance/TODo/script.js
@@ -0,0 +1,20 @@
+let task = document.getElementById('item');
+let input = document.querySelector('input')
+
+let btn = document.querySelector('button');
+let div = document.createElement('div');
+let dlt = document.createElement('button');
+dlt.setAttribute = "";
+
+btn.addEventListener('click', ()=>{
+
+    div.textContent = input.value;
+    task.appendChild(div);
+    dlt.textContent = "delete";
+    task.appendChild(dlt);
+    input.value = '';
+})
+
+dlt.addEventListener('click',(event)=>{
+    if(event.match === b)
+})
