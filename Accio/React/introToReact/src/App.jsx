a25646ed0b5c056a8bb4f44eb16193657720cd7d
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-07-18 18:38:45 +0530

    intro of react

---
+++ Accio/React/introToReact/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi eaque at error, fugit in totam. Ut cupiditate, veritatis quidem consequatur quos eligendi tempora.</p>
+    </>
+  )
+}
+
+export default App
