69d003459d3441f9eb610e1bfbe6d7bb856a8504
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-07-25 12:10:27 +0530

    Refactor App component and remove unused CSS files for improved clarity and functionality

--- Accio/React/9. ArrayStates/src/App.jsx
+++ Accio/React/9. ArrayStates/src/App.jsx
@@ -1,45 +1,42 @@
-import { use, useState } from 'react'
-
-
+import { useState } from "react";
 
 function App() {
-
   const [arr, setArr] = useState([]);
   const [sum, setSum] = useState(0);
   const [marks, setMarks] = useState("");
 
-  function handleSubmit(e){
+  function handleSubmit(e) {
     e.preventDefault();
     arr.push(marks);
     setArr([...arr]);
     setSum(sum + marks);
     setMarks("");
   }
+  // setArr((currentArr) => [...currentArr, marks]);
+  // setSum((currentSum) => currentSum + marks);
 
   return (
     <>
       <h1>Learning to handle arrays in React</h1>
 
       <form onSubmit={handleSubmit}>
-        <input placeholder='Enter Marks' value={marks} type="text" onChange={(e)=>setMarks(Number(e.target.value))}/>
+        <input
+          placeholder="Enter Marks"
+          value={marks}
+          type="number"
+          onChange={(e) => setMarks(Number(e.target.value))}
+        />
         <button>Add Marks</button>
       </form>
 
       <ul>
-        {
-          arr.map((score, index)=>{
-            return <li key={index}>{score}</li>
-          })
-        }
+        {arr.map((score, index) => {
+          return <li key={index}>{score}</li>;
+        })}
       </ul>
-
       <h2>Sum : {sum}</h2>
-
-
     </>
-  )
-
-  
+  );
 }
 
-export default App
+export default App;
