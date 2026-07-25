569b40d3acfe69b23e3d2827e9b46a347fd82915
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-07-25 11:22:43 +0530

    Remove unused CSS and asset files; refactor App component for clarity and improved functionality

--- Accio/React/7. props/src/App.jsx
+++ Accio/React/7. props/src/App.jsx
@@ -1,90 +1,100 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import Card from './Card.jsx'
-// import './App.css'
+import { useState } from "react";
+import Card from "./Card.jsx";
 
 function App() {
-
   const [count, setCount] = useState(0);
 
   const [name2, setname] = useState("");
 
   const isLoggedIn = true;
   const obj = {};
- 
-  let error = 0;
-  if(error){
-    return(
-      <>
-      <h1>502 Error</h1>
-      </>
-    )
-  }
 
+  let error = 0;
+  if (error) {
+    return (
+      <>
+        <h1>502 Error</h1>
+      </>
+    );
+  }
 
   const fruits = ["apple", "mango", "watermelon", "banana"];
 
-  function handleClick(name, e){
-    console.log("e", e.target)
+  function handleClick(name, e) {
+    console.log("e", e.target);
     console.log("Button Clicked by", name);
   }
-  
-  function add(a,b){
-    return a+b;
-  }
 
-  function printOnConsole(e){
+  function printOnConsole(e) {
     console.log("Printing", e.target);
-    setCount(count+1);
+    setCount(count + 1);
   }
-
-  // document.querySelector => not to use
-
-  // add(4,8)
 
   let name = "";
 
-  function handleInput(e){
+  function handleInput(e) {
     name = e.target.value;
-    setname(e.target.value)
+    setname(e.target.value);
   }
 
-  return(
-    
+  return (
     <div>
-        {/* <button onClick={handleClick("Azmat")} >Click Me</button> */}
-        <h1>{name}</h1>
+      <h1>{name}</h1>
+      <button
+        onClick={(e) => {
+          handleClick("Azmat", e);
+        }}
+      >
+        Click Me to print
+      </button>
 
-        <input type="text" onChange={handleInput} />
+      <input type="text" onChange={handleInput} />
 
-        <button onClick={(e)=>{
-          console.log();
-          let name = "Azmat";
-          handleClick(name, e);
-        }} >Click Me</button>
+      <button
+        onClick={(e) => {
+          handleClick("Azmat", e);
+        }}
+      >
+        Click Me
+      </button>
 
-        <button onClick={(e)=>{
+      <button
+        onClick={(e) => {
           console.log("Butn 2nd clicked", e.target);
-        }} > 2nd Button </button>
+        }}
+      >
+        {" "}
+        2nd Button{" "}
+      </button>
 
-        <button onClick={(e)=>{
+      <button
+        onClick={(e) => {
           printOnConsole(e);
-        }}>3rd Button</button>
+        }}
+      >
+        3rd Button
+      </button>
 
-    <h1>Learning React Rendering</h1>
-    {/* {isLoggedIn ? <Card name="Azmat" age={22} add={{}}/> : <button>Login/SignUP</button>} */}
-    {/* {obj && <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae, ea!</p>} */}
+      <h1>Learning React Rendering</h1>
+      {isLoggedIn ? (
+        <Card name="Azmat" age={22} add={45} />
+      ) : (
+        <button>Login/SignUP</button>
+      )}
+      {obj && (
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae,
+          ea!
+        </p>
+      )}
 
-    <ol>
-      {fruits.map((fruit, idx)=>(
-         <li key={idx}>{fruit}</li>
-      ))}
-    </ol>
-
-  </div>
-  )
+      <ol>
+        {fruits.map((fruit, idx) => (
+          <li key={idx}>{fruit}</li>
+        ))}
+      </ol>
+    </div>
+  );
 }
 
-export default App
+export default App;
