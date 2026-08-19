a67a98b8f3074710b1f541c27ced8687404678dd
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-08-19 18:20:44 +0530

    feat: initialize react-router project with basic structure and routing
    - Added package.json with dependencies for React, React Router, and Vite.
    - Created main application component (App.jsx) with routing setup.
    - Implemented Navbar component for navigation using NavLink.
    - Added pages: Home, Contact, Login, Product, and ResourceNotFound.
    - Included basic styling for active navigation links.
    - Configured Vite for development and build processes.

---
+++ Accio/React/32. react-router/src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+        Call to +91 1234567890
+    </div>
+  )
+}
+
+export default Contact
---
+++ Accio/React/32. react-router/src/pages/ResourceNotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ResourceNotFound() {
+  return (
+    <>
+    <h1>Error : 404 - Resource Not found</h1>
+    </>
+  )
+}
+
+export default ResourceNotFound
