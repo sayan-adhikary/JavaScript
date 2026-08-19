f0f82b1fdbfd72cab4c0adf019243e6ec27b4dcf
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-08-19 18:03:02 +0530

    Add initial React setup with components, styles, and assets
    - Created main application structure with App.jsx and Card.jsx components.
    - Added CSS styles in App.css for layout and design.
    - Included SVG icons in public/icons.svg for use in the application.
    - Added assets including hero image and logos for React and Vite.
    - Configured Vite for development with vite.config.js.
    - Established index.css for global styles and main.jsx for rendering the app.

---
+++ Accio/React/28. children/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Card from './Card.jsx'
+function App() {
+  return (
+    <>
+      <Card>
+        Messi
+        {/* <div>name : Azmat</div> */}
+        {/* <a href="https://www.chatgpt.com">GPT</a> */}
+      </Card>
+
+      {/* <Card /> */}
+    </>
+  )
+}
+
+export default App
