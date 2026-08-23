3e68510b983f010f1862f38c76dffbe21359e519
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-08-23 12:39:18 +0530

    feat: initialize Counter Dashboard project with React and Vite
    - Add package.json with dependencies and scripts for development
    - Create App component with counter functionality (increase, decrease, reset)
    - Set up main entry point for React application
    - Configure Vite for project build and development

---
+++ Accio/Interview Practice/Machine Coding/Counter Dashboard/src/App.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+
+  function handleClick(){
+    if(count - step < 0){
+      setCount(0);
+    } else {
+      setCount(count - step);
+    }
+  }
+  
+  return (
+    <>
+      <div
+        style={{ backgroundColor: "black", color: "white", minHeight: "100vh" }}
+      >
+        <h1>Counter Dashboard</h1>
+        <h2>{count}</h2>
+        <button onClick={() => setCount((prev) => prev + step)}>
+          increase
+        </button>
+        <button onClick={handleClick} disabled={count < 0}>
+          decrease
+        </button>
+        <button onClick={() => setCount(0)}>Reset</button>
+        <input
+          type="number"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+      </div>
+    </>
+  );
+}
+
+export default App;
