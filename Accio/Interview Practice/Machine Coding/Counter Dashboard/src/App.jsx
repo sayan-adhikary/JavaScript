dbee15a7caa84a7be8a1eee3e1d42f472972a76f
sayan-adhikary <103528635+sayan-adhikary@users.noreply.github.com>
2026-08-27 18:40:09 +0530

    refactor: rename handleClick to handleDecrease for clarity and consistency

--- Accio/Interview Practice/Machine Coding/Counter Dashboard/src/App.jsx
+++ Accio/Interview Practice/Machine Coding/Counter Dashboard/src/App.jsx
@@ -1,29 +1,29 @@
-import React from "react";
-import { useState } from "react";
+import React from 'react';
+import { useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
 
-  function handleClick(){
-    if(count - step < 0){
+  function handleDecrease() {
+    if (count - step < 0) {
       setCount(0);
     } else {
       setCount(count - step);
     }
   }
-  
+
   return (
     <>
       <div
-        style={{ backgroundColor: "black", color: "white", minHeight: "100vh" }}
+        style={{ backgroundColor: 'black', color: 'white', minHeight: '100vh' }}
       >
         <h1>Counter Dashboard</h1>
         <h2>{count}</h2>
         <button onClick={() => setCount((prev) => prev + step)}>
           increase
         </button>
-        <button onClick={handleClick} disabled={count < 0}>
+        <button onClick={handleDecrease} disabled={count < 0}>
           decrease
         </button>
         <button onClick={() => setCount(0)}>Reset</button>
